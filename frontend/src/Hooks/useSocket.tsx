d71d605ad271f825ce6ddb813b596fb8243f035e
Trishnak-K <itstrishank@gmail.com>
2025-01-16 21:34:36 +0530

    Added WebSocket URL

--- frontend/src/Hooks/useSocket.tsx
+++ frontend/src/Hooks/useSocket.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-const WS_URL = "ws://localhost:8080/";
+const WS_URL = "ws://20.244.91.156:8080/";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
